2134b165b79cc455f5f863156f961b40ee957def
sumin305 <gogotnals@naver.com>
2024-12-10 01:55:02 +0900

    feat: 메인 화면 layout 구현

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,5 +1,12 @@
+import TodoList from "../todolist/TodoList";
 import styles from "./main.module.css";
+import TodoInput from "@/components/todoInput/todoInput";
 
 export default function Main() {
-  return <div className={styles.layout}></div>;
+  return (
+    <div className={styles.layout}>
+      <TodoInput></TodoInput>
+      <TodoList></TodoList>
+    </div>
+  );
 }
