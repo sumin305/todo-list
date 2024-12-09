56233abc0f83243c8e1dcf2f57cb9bd015ce0c45
sumin305 <gogotnals@naver.com>
2024-12-09 21:45:21 +0900

    feat: 메인 화면 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Link from "next/link";
 import Header from "@/components/header/Header";
+import Main from "@/components/main/main";
 
 export const metadata: Metadata = {
   title: "todo list",
@@ -17,29 +18,7 @@ export default async function RootLayout({
     <html>
       <body>
         <Header></Header>
-        <h1>
-          <Link href="/">WEB</Link>
-        </h1>
-        <ol>
-          <li>
-            <Link href="/read/1">html</Link>
-          </li>
-          <li>
-            <a href="/read/2">css</a>
-          </li>
-        </ol>
-        {children}
-        <ul>
-          <li>
-            <Link href="/create">Create</Link>
-          </li>
-          <li>
-            <Link href="/update/1">Update</Link>
-          </li>
-          <li>
-            <input type="button" value="delete"></input>
-          </li>
-        </ul>
+        <Main></Main>
       </body>
     </html>
   );
---
+++ src/components/main/main.tsx
@@ -0,0 +1,5 @@
+import styles from "./main.module.css";
+
+export default function Main() {
+  return <div className={styles.layout}></div>;
+}
